d11208b4a1a16774e706af89fa58ceb43d8db542
Dana Al-Abbade <abbadidana91@gmail.com>
2020-08-21 23:44:48 +0300

    Test for edge cases

--- tests/challenges/test_array_shift.py
+++ tests/challenges/test_array_shift.py
@@ -1,22 +1,27 @@
-from data_structures_and_algorithms.challenges.array_shift.array_shift import insertShiftArray
+from data_structures_and_algorithms.challenges.array_shift.array_shift import insert_shift_array
 
-def testHappyPath():
-    actual=insertShiftArray([1,2,4,5], 3)
+def test_happy_path():
+    actual=insert_shift_array([1,2,4,5], 3)
     expected=[1,2,3,4,5]
     assert actual == expected
 
-def testExpectedFailure():
-    actual=insertShiftArray([5,2,7,3], 3)
+def test_expected_failure():
+    actual=insert_shift_array([5,2,7,3], 3)
     expected=[3,5,2,7,3]
     assert actual == expected
 
 
-def testExpectedFailure2():
-    actual=insertShiftArray([2,2,2,2], 5)
+def test_expected_failure2():
+    actual=insert_shift_array([2,2,2,2], 5)
     expected=[2,2,2,5,2]
     assert actual == expected
 
-def testEdgeCase():
-    actual=insertShiftArray([0,0,9,9], 5)
-    expected=[0,0,5,9,9]
+def test_edge_case():
+    actual=insert_shift_array([], 5)
+    expected=[5]
     assert actual == expected
+
+def test_edge_case2():
+    actual=insert_shift_array([1,2,4], '5')
+    expected="Enter a number to shift the list [1, 2, 4]"
+    assert actual == expected 
