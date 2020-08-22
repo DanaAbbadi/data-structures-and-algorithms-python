26c886d80a4cd1c783e7db4be1285c509d364f48
Dana Al-Abbade <abbadidana91@gmail.com>
2020-08-22 23:14:49 +0300

    done linked lists

---
+++ data_structures_and_algorithms/data_structures/linked_list/README.md
@@ -0,0 +1,19 @@
+# Singly Linked List
+
+## Problem Domain
+
+To create a single linked list, with the ability to insert any new node and search for a certain node value
+
+## Challenge
+
+The challenge was mainly about accessing the linked list and move between nodes
+
+
+## Methods
+
+##### The methods used are:
+
+1. **Insert** method which takes any value as an argument and adds a new node with that value to the head of the list.
+2. **__str__** method which takes in no arguments and returns a string representing all the values in the Linked List.
+
+3. **Includes** method which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,64 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+    
+    def __str__(self): 
+        return self.value
 
-    # put your LinkedList implementation here
-    pass
+class LinkedList :
+
+    def __init__(self):
+        self.head = None
+   
+    def insert(self,value):
+        new_node = Node(value)
+        if not self.head:
+            self.head=new_node
+        else:
+           new_node.next = self.head
+           self.head = new_node    
+   
+    def append(self,value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+  
+  
+    def __str__(self):  
+        current = self.head
+        output = ''
+        while current:
+            output += f"{current.value}->"
+            current = current.next
+        output+=" NULL"
+        return output 
+   
+   
+   
+    def includes (self,value):
+        if self.head == None:
+            return False
+        else:
+            current = self.head
+            while current:
+                if current.value == value:
+                    return True
+                else :
+                    current = current.next    
+            return False        
+
+                        
+                
+if __name__ == "__main__":
+    fruits = LinkedList()
+    fruits.append('apple')
+    fruits.insert('banana')
+    fruits.append('orange')
+    print( fruits.includes('banana'))
+    print(fruits)
