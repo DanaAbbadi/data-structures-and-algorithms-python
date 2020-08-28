b35475e52f399bc6aacbbef2c8529e8cdae4552d
Dana Al-Abbade <abbadidana91@gmail.com>
2020-08-28 23:37:17 +0300

    done stack and queue

---
+++ data_structures_and_algorithms/stacks_and_queues_challenges/README.md
@@ -0,0 +1,45 @@
+# Stacks and Queues
+
+
+## Description
+
+Stacks and Queues are pure computer science fundamentals that every developer needs to understand. And so, the best way to illustrate any data structure concept is by building it from scratch, thus I built both a Stack class and Queue class with their methods. Also, the approach of FILO and FIFO can be really tricky but by building their corresponding methods, I got the hang of it.
+
+If you are still new to this concept, you can check out my notes [here](https://danaabbadi.github.io/Reading-Notes-for-Advanced-Software-Development-in-Python-Course/class10).
+
+
+## Challenge
+
+The challenge was mainly to distinguish between FIFO and FILO and also to utilize the Single-responsibility principle when writting the methods.
+
+
+## Whiteboard
+
+**Stack**
+
+![stack](/assets/stack.PNG)
+
+**Queue**
+
+![queue](/assets/queue.PNG)
+
+
+
+## Methods
+
+##### Stack methods:
+
+* **push()** – Adds a new node at the top of the stack 
+* **pop()**– Deletes the top most element of the stack 
+* **Peek()** - When you peek you will view the value of the top Node in the stack.
+* **IsEmpty** - returns true when stack is empty otherwise returns false.
+
+
+### Queue methods:
+
+* **Enqueue** - Nodes or items that are added to the queue.
+* **Dequeue** - Nodes or items that are removed from the queue. If called when the queue is empty an exception will be raised.
+* **Peek** - When you peek you will view the value of the front Node in the queue. If called when the queue is empty an exception will be raised.
+* **IsEmpty** - returns true when queue is empty otherwise returns false.
+
+   
---
+++ data_structures_and_algorithms/stacks_and_queues_challenges/queue.py
@@ -0,0 +1,90 @@
+from data_structures_and_algorithms.stacks_and_queues_challenges.node import Node
+# from node import Node
+
+
+class Queue(Node):
+
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self,*args):
+        """
+        Takes a single or multiple values as an argument and adds the new nodes with that value to the back of the queue. 
+
+        Arguments:
+            *args -- list of values with variable length
+        """
+        values=[]
+        if not self.front and not self.rear:
+            node = Node(args[0])
+            self.front = node
+            self.rear = node
+            values = args[1:]
+        else:
+            values = args
+        for i in values:
+            temp = self.rear
+            self.rear = Node(i)
+            temp.next = self.rear
+
+       
+        
+
+    def dequeue(self):
+        """
+        * Removes the node from the front of the queue, and returns the node’s value. 
+        * Will raise an exception if the queue is empty.
+        """
+        try:
+            value = self.front.value
+            if self.front == self.rear:
+                self.rear = None
+            self.front = self.front.next
+
+            return value
+        
+        except AttributeError as error:
+            return 'Queue is empty'
+    
+    def peek(self):
+        """
+        * Returns the value of the node located in the front of the queue, without removing it from the queue.
+        * Will raise an exception if the queue is empty.
+        """
+        try:
+            return self.front.value
+        except AttributeError as error:
+            return 'Queue is empty'
+    
+    def isEmpty(self):
+        """
+        Checks whether or not the Queue is empty.
+        """
+        return False if self.front else True
+        
+
+    def __str__(self):
+        current = self.front
+        output = 'front->'
+        while current:
+            output += f"{current.value}->" 
+            current = current.next
+        output+=" rear"
+        return output 
+
+
+
+if __name__ == "__main__":
+    nums = Queue()
+    nums.enqueue(1)
+    nums.enqueue(2,4)
+    nums.enqueue(55,5)
+
+    print(nums.dequeue())
+    print(nums)
+    print(nums.dequeue())
+    print(nums.peek())
+    print(nums.isEmpty())
+
+  
